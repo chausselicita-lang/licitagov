8650a368e2cadcae9ac6c12d1f57aa3619020411
Cleriston <chausselicita@gmail.com>
2026-06-13 10:03:24 -0300

    fix: skipWaiting e clientsClaim no SW para atualização imediata de cache

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,8 @@ export default defineConfig({
         ],
       },
       workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,jpeg,jpg,woff2}'],
         navigateFallback: '/',
         navigateFallbackDenylist: [/^\/api/],
